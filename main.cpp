1935d6236ea3d3b83623567868c59a6f82465434
ckron <7393106+ckron@users.noreply.github.com>
2020-06-07 12:52:36 +0900

    add loadKeys()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
+#include <string>
 
 #include "opencv2/opencv.hpp"
+
+cv::Mat keys[10];
+
+int pickupKabukaInImage(cv::Mat src)
+{
+
+	return 0;
+}
+
+void loadKeys()
+{
+	for(int i=0; i<10; i++) {
+		std::string filename = "./img/00_key/" + std::to_string(i) + ".jpg";
+
+		//std::cout << filename << std::endl;
+
+		keys[i] = cv::imread(filename.c_str());
+	}
+}
 
 std::vector<cv::Point> find(cv::Mat src, cv::Mat tempL, float threshold)
 {
@@ -29,19 +49,17 @@ std::vector<cv::Point> find(cv::Mat src, cv::Mat tempL, float threshold)
 int main(int argc, char* argv[])
 {
 	cv::Mat mat = cv::imread("img/sample/domori.jpg");
-	cv::Mat key = cv::imread("img/00_key/8.jpg");
+	loadKeys();
 	
-	std::cout << mat.depth() << std::endl;
-
-	std::vector<cv::Point> locs = find(mat, key, 0.95);
+	std::vector<cv::Point> locs = find(mat, keys[0], 0.95);
 
 	std::cout << locs.size() << std::endl;
 	for(auto ml : locs) {
-		cv::rectangle(mat, cv::Rect(ml.x, ml.y, key.cols, key.rows),
+		cv::rectangle(mat, cv::Rect(ml.x, ml.y, keys[0].cols, keys[0].rows),
 										cv::Scalar(0, 0, 255));
 	}
 
-	cv::imshow("test", mat);
+	cv::imshow("image", mat);
 	
 	cv::waitKey();
 
