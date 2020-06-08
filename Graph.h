9b0ce82a6e91684eae4b810d5edeb888447a4994
ckron <7393106+ckron@users.noreply.github.com>
2020-06-08 17:55:04 +0900

    show label

--- Graph.h
+++ Graph.h
@@ -24,6 +24,7 @@ public:
 
 		drawAxis(res);
 		drawPoints(res);
+		drawLabel(res);
 
 		return res;
 	}
@@ -38,41 +39,69 @@ private:
 	static const int USE_BLACK_COLOR = 10;
 	static const int DRAWAREA_COLOR = 250;
 
-	void drawPoints(cv::Mat dst)
+	static const int VALUE_TENJO = 800;
+
+	void drawLabel(cv::Mat img)
 	{
-		cv::Point xAxisStart(GRAPH_MARGIN, dst.rows - GRAPH_MARGIN);
-		cv::Point xAxisEnd(dst.cols - GRAPH_MARGIN, xAxisStart.y);
+		cv::Point yAxisTopLabel(GRAPH_MARGIN - 10, GRAPH_MARGIN);
+		cv::Point yAxisDownLabel(GRAPH_MARGIN - 10, img.rows - GRAPH_MARGIN);
+
+		cv::putText(img, std::to_string(VALUE_TENJO), cv::Point(yAxisTopLabel),
+								cv::FONT_HERSHEY_SIMPLEX, 0.5,
+								cv::Scalar::all(USE_BLACK_COLOR));
+		cv::putText(img, std::to_string(0), cv::Point(yAxisDownLabel),
+								cv::FONT_HERSHEY_SIMPLEX, 0.5,
+								cv::Scalar::all(USE_BLACK_COLOR));
+	}
+
+	cv::Point adjustPos(int val, int max, int min,
+												cv::Point maxPos, cv::Point minPos)
+	{
+		double d = max - min;
+		double m = (val - min) / d;
+		cv::Point vec = maxPos - minPos;
+
+		return minPos + (vec * m);
+	}
+
+	void drawPoints(cv::Mat img)
+	{
+		cv::Point xAxisStart(GRAPH_MARGIN, img.rows - GRAPH_MARGIN);
+		cv::Point xAxisEnd(img.cols - GRAPH_MARGIN, xAxisStart.y);
 		cv::Point yAxisStart(GRAPH_MARGIN, GRAPH_MARGIN);
-		cv::Point yAxisEnd(yAxisStart.x, dst.rows - GRAPH_MARGIN);
+		cv::Point yAxisEnd(yAxisStart.x, img.rows - GRAPH_MARGIN);
 		
 		for(int i=0; i<values->size(); i++) {
-			int x = xAxisStart.x + 100 + 10 * i;
-			int y = yAxisEnd.y - (*values)[i];
+			cv::Point p = adjustPos((*values)[i], VALUE_TENJO, 0,
+																yAxisStart, yAxisEnd);
+			p.x = xAxisStart.x + 100 + 10 * i;
 
-			cv::circle(dst, cv::Point(x, y), 10, cv::Scalar(0, 0, 255));
+			cv::circle(img, p, 3, cv::Scalar(0, 0, 255));
 		}
 
 		for (int i=0; i<values->size()-1; i++) {
-			int x = xAxisStart.x + 100 + 10 * i;
-			int y = yAxisEnd.y - (*values)[i];
-			int nx = xAxisStart.x + 100 + 10 * (i+1);
-			int ny = yAxisEnd.y - (*values)[i+1];
+			cv::Point p = adjustPos((*values)[i], VALUE_TENJO, 0,
+															yAxisStart, yAxisEnd);
+			p.x = xAxisStart.x + 100 + 10 * i;
 
-			cv::line(dst, cv::Point(x, y), cv::Point(nx, ny),
-								cv::Scalar(0, 0, 255));
+			cv::Point np = adjustPos((*values)[i+1], VALUE_TENJO, 0,
+															yAxisStart, yAxisEnd);
+			np.x = xAxisStart.x + 100 + 10 * (i+1);
+
+			cv::line(img, p, np, cv::Scalar(0, 0, 255));
 		}
 	}
 
-	void drawAxis(cv::Mat dst)
+	void drawAxis(cv::Mat img)
 	{
-		cv::Point xAxisStart(GRAPH_MARGIN, dst.rows - GRAPH_MARGIN);
-		cv::Point xAxisEnd(dst.cols - GRAPH_MARGIN, xAxisStart.y); 
+		cv::Point xAxisStart(GRAPH_MARGIN, img.rows - GRAPH_MARGIN);
+		cv::Point xAxisEnd(img.cols - GRAPH_MARGIN, xAxisStart.y);
 		cv::Point yAxisStart(GRAPH_MARGIN, GRAPH_MARGIN);
-		cv::Point yAxisEnd(yAxisStart.x, dst.rows - GRAPH_MARGIN);
+		cv::Point yAxisEnd(yAxisStart.x, img.rows - GRAPH_MARGIN);
 
 		// draw X Axis.
-		line(dst, xAxisStart, xAxisEnd, cv::Scalar::all(USE_BLACK_COLOR));
+		line(img, xAxisStart, xAxisEnd, cv::Scalar::all(USE_BLACK_COLOR));
 		// draw Y Axis.
-		line(dst, yAxisStart, yAxisEnd, cv::Scalar::all(USE_BLACK_COLOR));
+		line(img, yAxisStart, yAxisEnd, cv::Scalar::all(USE_BLACK_COLOR));
 	}
 };
